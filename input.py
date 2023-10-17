81a5963ff2ac785479c48344e6ec03626d594581
unknown <you@example.com>
2023-10-17 16:59:17 +0200

    adding

---
+++ input.py
@@ -0,0 +1,3 @@
+numbers = input("Give me a number = ")
+
+print(numbers)
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 def add(x, y):
-    pass    
+    return x+y 
 
 def subtract(x, y):
-    return 0
+    return x-y  
+
+def multiply(x, y): 
+    return x*y
  
-
 def division(x, y):
-    pass
-    
+    return x/y
 
-def multiply(x, y):
-    pass    
+def user_input():
+    number1 = int(input("Give me a number : "))
+    number2 = int(input("Give me another number : "))
+    return number1, number2
+
+user_info = user_input()
+# 2 4
+
+print(add(user_info[0], user_info[1]))
+# print(subtract(user_input()))
+# print(multiply(user_input()))
+# print(division(user_input()))
