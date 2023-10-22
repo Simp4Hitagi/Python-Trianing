e803a67347ce655f04788f11c698848cb9c651a1
Simp4Hitagi <denorauten@gmail.com>
2023-10-22 22:27:09 +0200

    added split function, while loop for user input, if condition for quiting, casefold function for case sensitivity

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 def get():
-    user_x = int(input('Enter x: '));
-    user_y = int(input('Enter y: '));
+    #enter 2 digits seperated by space: 
+    user_x, user_y = input("Enter two values separated by space: ").split(' ')
+
+    if user_x or user_y != int():
+            #keep asking for input if the input is not digits
+            while True:
+                print('Enter only numbers');
+                user_x, user_y = input("Enter two values separated by space: ").split(' ');
+    
+        #and if the user enters the word "quit" then the program must stop
+        #the quit shouldn't be case sensetive
+    elif user_x or user_y == (str("quit")).casefold():
+        print('stuff')
+
+
+    print(user_x, user_y)
     return user_x, user_y
 
 def add(x, y):
@@ -16,13 +30,13 @@ def multiply(x, y):
     return x*y
 
 
+#create a "display results function"
+def display_results():
+    return 
+
 results = get()
 
 print("Addition: ", add(results[0], results[1]))
 print("Subtract: ", subtract(results[0], results[1]))
 print("Division: ", division(results[0], results[1]))
 print("Multiply: ", multiply(results[0], results[1]))
-
-
-
-
